53b0300f5da8fd422315514c826da78dd90cfb96
9182092380 <pothulasharath452@gmail.com>
2024-05-16 12:27:02 +0530

    secound commit

--- src/main/java/org/wipro/Employeeapplication/Employee.java
+++ src/main/java/org/wipro/Employeeapplication/Employee.java
@@ -6,37 +6,44 @@ public class Employee {
     private String designation;
     private double salary;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
+    public Employee(int id, String name, String designation, double salary) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public String getDesignation() {
-        return designation;
-    }
-
-    public void setDesignation(String designation) {
         this.designation = designation;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
         this.salary = salary;
     }
+
+    //    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getDesignation() {
+//        return designation;
+//    }
+//
+//    public void setDesignation(String designation) {
+//        this.designation = designation;
+//    }
+//
+//    public double getSalary() {
+//        return salary;
+//    }
+//
+//    public void setSalary(double salary) {
+//        this.salary = salary;
+//    }
     void display()
     {
         System.out.println("Id:"+id);
